b2f54b534d937fe6564a76c8dafa4a4657e15494
HyunjoonCho <jhj09016@gmail.com>
2021-11-14 10:29:16 +0900

    [#2] Crawl based on date

--- crawler.py
+++ crawler.py
@@ -5,28 +5,14 @@ import pandas as pd
 import requests
 import datetime
 
-def crawl(companyName, isKor=True): 
-    options = webdriver.ChromeOptions()
-    options.headless = True
-    options.add_argument('window-size=1920x1080')
-    options.add_argument("disable-gpu")
-
-    driver = webdriver.Chrome('chromedriver', options=options)
-
-    if isKor:
-        country = ('ko', 'KR')
-        print(f'Start crawling for {companyName} in Google News Korea')
-    else:
-        country = ('en', 'US')
-        print(f'Start crawling for {companyName} in Google News US')
-
-    url = f'https://news.google.com/rss/search?q={companyName}+when:1d&hl={country[0]}&gl={country[1]}&ceid={country[1]}:{country[0]}'
+def loadData(driver, url):
     driver.get(url)
     driver.implicitly_wait(3)
     url = driver.current_url
     resp = requests.get(url)
-    soup = bs(resp.text, 'xml')
+    return bs(resp.text, 'xml')
 
+def parseToDataFrame(soup):
     titles = []
     links = []
     pubDates = []
@@ -46,15 +32,43 @@ def crawl(companyName, isKor=True):
         descriptions.append(item.description.string.split('>')[1].split('<')[0])
 
     data = {'title': titles, 'link': links, 'pubDate': pubDates, 'description': descriptions}
-    data_frame = pd.DataFrame(data, columns=['title', 'link', 'pubDate', 'description'])
-    data_frame.to_csv(f'./news/{country[1]}/{companyName}.csv')
-    print(f'Crawled {len(titles)} articles!')
+    return pd.DataFrame(data, columns=['title', 'link', 'pubDate', 'description']), len(titles)
+
+def crawl(companyName, startDate, endDate, isKor=True): 
+    if isKor:
+        country = ('ko', 'KR')
+        print(f'Start crawling for {companyName} in Google News Korea')
+    else:
+        country = ('en', 'US')
+        print(f'Start crawling for {companyName} in Google News US')
+    
+    currentDate = startDate
+    totalCount = 0
+    while currentDate != endDate:
+        nextDate = currentDate + datetime.timedelta(days=1)
+        url = f'https://news.google.com/rss/search?q={companyName}+after:{currentDate.isoformat()}+before:{nextDate.isoformat()}& \
+                hl={country[0]}&gl={country[1]}&ceid={country[1]}:{country[0]}'
+        soup = loadData(driver, url)
+        df, dailyCount = parseToDataFrame(soup)
+        totalCount += dailyCount
+        df.to_csv(f'./news/{country[1]}/{companyName}_{currentDate.isoformat()}.csv')
+        print(f'  {currentDate.isoformat()}: {dailyCount} articles')
+        currentDate = nextDate
+    print(f'Crawled {totalCount} articles in total!')
 
 if __name__=="__main__":
-    companyListK = ['삼성전자', '한국조선해양', '하이트진로', '쿠콘']
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    driver = webdriver.Chrome('chromedriver', options=options)
+
+    startDate = datetime.date(2020, 3, 3) # inclusive
+    endDate = datetime.date(2020, 3, 10) # exclusive
+    companyListK = ['삼성전자', '한국조선해양', '신세계']
     for companyName in companyListK:
-        crawl(companyName, True)
+        crawl(companyName, startDate, endDate)
 
     companyListUS = ['Apple', 'IBM', 'Delta Air Lines']
     for companyName in companyListUS:
-        crawl(companyName, False)
+        crawl(companyName, startDate, endDate, False)
